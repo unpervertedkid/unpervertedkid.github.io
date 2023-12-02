5291031b851c2b717e62f27a0536eb46bbc1a863
Brian Silah <71752651+unpervertedkid@users.noreply.github.com>
2023-12-02 12:07:06 +0000

    Add unoptimized images option to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
 }
-
 module.exports = nextConfig
