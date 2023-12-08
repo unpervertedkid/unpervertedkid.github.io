09703db5b61939b0298cfbed89ef694417c1f851
Brian Silah <71752651+unpervertedkid@users.noreply.github.com>
2023-12-08 13:20:37 +0000

    feat: Added homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,110 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import * as React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const Email = "brianelijahsilah@gmail.com";
+const Linkedin = "https://www.linkedin.com/in/briansilah/";
+const Github = "https://github.com/unpervertedkid";
+const Resume = "https://docs.google.com/document/d/1zsrxP9fLozn-pqbv6muNRUILbcF1h36lApBIfJ_pGL0/edit?usp=sharing";
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-white flex flex-col pl-20 pr-16 pt-6 max-md:px-5">
+      <div className="items-stretch flex justify-between gap-5 self-end max-md:justify-center">
+        <div className="text-black text-xl whitespace-nowrap">
+          <a href={Linkedin} target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </div>
+        <div className="text-black text-xl">
+          <a href={Github} target="_blank" rel="noopener noreferrer">Github</a>
+        </div>
+        <div className="text-black text-xl whitespace-nowrap">
+          <a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="text-black text-7xl font-extrabold max-w-[523px] ml-40 mt-20 self-start max-md:max-w-full max-md:text-4xl max-md:mt-10">
+        Hey,
+        <br />
+        I’m Brian Silah
+      </div>
+      <div className="text-black text-opacity-70 text-5xl font-extrabold self-center max-w-[693px] ml-20 mt-7 max-md:max-w-full max-md:text-4xl">
+        — a curious and passionate software engineer who thrives on finding
+        solutions to complex and challenging problems with a collaborative
+        mindset.
+      </div>
+      <img
+        loading="lazy"
+        src="/Image.png"
+        className="aspect-[2.03] object-contain object-center w-full overflow-hidden self-center max-w-[1175px] mt-48 max-md:max-w-full max-md:mt-10"
+      />
+      <div className="text-black text-7xl font-extrabold max-w-[954px] ml-40 mt-40 self-start max-md:max-w-full max-md:text-4xl max-md:mt-10">
+        Here’s what you can expect from me....
+      </div>
+      <div className="items-end border self-center flex w-[954px] max-w-full flex-col mt-28 pl-16 pr-20 py-12 rounded-[40px] border-solid border-black border-opacity-40 max-md:mt-10 max-md:px-5">
+        <div className="text-black text-opacity-80 text-4xl font-bold self-stretch max-w-[832px] pb-5 mr-5 max-md:max-w-full max-md:mr-2.5">
+          Quick Learner
+        </div>
+        <div className="text-black text-3xl self-stretch max-w-[774px] mr-5 max-md:max-w-full max-md:mr-2.5">
+          I bring a fresh perspective and a growth mindset, eager to learn new
+          skills, technologies, and domains, and adapt to changing requirements
+          and environments.
+        </div>
+        <img
+          loading="lazy"
+          src="/divider.svg"
+          className="aspect-[261] object-contain object-center w-full stroke-[1px] stroke-black stroke-opacity-40 overflow-hidden self-stretch mr-5 mt-6 max-md:max-w-full max-md:mr-2.5"
         />
+        <div className="text-black text-opacity-80 text-4xl font-bold self-stretch max-w-[832px] pb-5 mr-5 mt-6 max-md:max-w-full max-md:mr-2.5">
+          Dot Connector
+        </div>
+        <div className="text-black text-3xl self-stretch max-w-[774px] mr-5 max-md:max-w-full max-md:mr-2.5">
+          I join your team with conviction and openness, ready to listen to your
+          needs, challenge your assumptions, and reconnect the dots to find new
+          opportunities and solutions.
+        </div>
+        <img
+          loading="lazy"
+          src="/divider.svg"
+          className="aspect-[261] object-contain object-center w-full stroke-[1px] stroke-black stroke-opacity-40 overflow-hidden self-stretch mr-5 mt-14 max-md:max-w-full max-md:mr-2.5 max-md:mt-10"
+        />
+        <div className="text-black text-opacity-80 text-4xl font-bold self-stretch max-w-[832px] pb-5 mr-5 mt-6 max-md:max-w-full max-md:mr-2.5">
+          Detail and Perfomance Oriented
+        </div>
+        <div className="text-black text-3xl self-stretch max-w-[774px] mr-5 max-md:max-w-full max-md:mr-2.5">
+          I ensure quality and efficiency in every aspect of my work, from
+          design to implementation to testing, while balancing time and other
+          constraints. I strive to optimize the performance of my code, systems,
+          and products.
+        </div>
+        <img
+          loading="lazy"
+          src="/divider.svg"
+          className="aspect-[261] object-contain object-center w-full stroke-[1px] stroke-black stroke-opacity-40 overflow-hidden self-stretch mr-5 mt-6 max-md:max-w-full max-md:mr-2.5"
+        />
+        <div className="text-black text-opacity-80 text-4xl font-bold max-w-[832px] pb-5 mt-6 self-start max-md:max-w-full">
+          Complex Problem Solver
+        </div>
+        <div className="text-black text-3xl max-w-[774px] self-start max-md:max-w-full">
+          I use creativity and logic to find optimal solutions to problems that
+          require complex and challenging thinking. I enjoy exploring new
+          possibilities and pushing the boundaries of what is possible.
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="text-black text-center text-3xl self-center max-w-[466px] mt-32 max-md:max-w-full max-md:mt-10">
+        Sounds interesting, 
+        <a href={`mailto:${Email}`} className="underline">reach out</a> 
+        and lets connect!
       </div>
-    </main>
-  )
+      <div className="items-stretch self-center flex justify-between gap-5 mt-40 pb-10 max-md:justify-center max-md:mt-10">
+        <div className="text-black text-xl whitespace-nowrap">
+          <a href={Linkedin} target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </div>
+        <div className="text-black text-xl">
+          <a href={Github} target="_blank" rel="noopener noreferrer">Github</a>
+        </div>
+        <div className="text-black text-xl whitespace-nowrap">
+          <a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+
