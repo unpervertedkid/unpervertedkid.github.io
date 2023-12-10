c3bd45a0782b6e792c9a331a7d1f2249cf97026d
Brian Silah <unpervertedkid@gmail.com>
2023-12-10 18:58:18 +0300

    refactor: Change styling of connectioninvite section

--- src/components/ConnectInvite.jsx
+++ src/components/ConnectInvite.jsx
@@ -1,9 +1,14 @@
-export default function ConnectInvite({Email}) {
-    return(
-        <div className="text-black text-center text-3xl self-center max-w-[466px] mt-32 max-md:max-w-full max-md:mt-10">
-            Sounds interesting,
-            <a href={`mailto:${Email}`} className="underline">reach out</a>
-            and lets connect!
-        </div>
-    );
+export default function ConnectInvite({ Email }) {
+  return (
+    <div className="text-black text-center text-3xl self-center max-w-[466px] mt-32 max-md:max-w-full max-md:mt-10">
+      <span className="">Sounds interesting, </span>
+      <span className="font-bold">
+        <a href={`mailto:${Email}`} className="underline">
+          reach out
+        </a>
+      </span>
+      <span className="font-bold"> </span>
+      <span className="">and lets connect!</span>
+    </div>
+  );
 }
