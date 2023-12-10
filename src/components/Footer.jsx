cfbaf29d13ca66dc8993778717636269978865a9
Brian Silah <unpervertedkid@gmail.com>
2023-12-10 19:16:10 +0300

    refactor(footer): Added hover color to links, reduced top padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,38 @@
-export default function Footer({Linkedin, Github, Resume}) {
-    return(
-        <>
-            <div className="items-stretch self-center flex justify-between gap-5 mt-40 pb-10 max-md:justify-center max-md:mt-10">
-                <div className="text-black text-xl whitespace-nowrap">
-                    <a href={Linkedin} target="_blank" rel="noopener noreferrer">Linkedin</a>
-                </div>
-                <div className="text-black text-xl">
-                    <a href={Github} target="_blank" rel="noopener noreferrer">Github</a>
-                </div>
-                <div className="text-black text-xl whitespace-nowrap">
-                    <a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
-                </div>
-            </div>
-        </>
-    );
+export default function Footer({ Linkedin, Github, Resume }) {
+  return (
+    <>
+      <div className="items-stretch self-center flex justify-between gap-5 mt-20 pb-10 max-md:justify-center max-md:mt-10">
+        <div className="text-black text-xl whitespace-nowrap">
+          <a
+            href={Linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-brown"
+          >
+            Linkedin
+          </a>
+        </div>
+        <div className="text-black text-xl">
+          <a
+            href={Github}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-brown"
+          >
+            Github
+          </a>
+        </div>
+        <div className="text-black text-xl whitespace-nowrap">
+          <a
+            href={Resume}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-brown"
+          >
+            Resume
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
