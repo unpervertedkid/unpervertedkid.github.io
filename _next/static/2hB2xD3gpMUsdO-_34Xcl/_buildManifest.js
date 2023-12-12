d3eed4937161e27f489ba3885a2773a7289e66b5
unpervertedkid <unpervertedkid@users.noreply.github.com>
2023-12-12 09:39:08 +0000

    deploy: 374c5d2c790366e101f60a14e29489625e6f5298

---
+++ _next/static/2hB2xD3gpMUsdO-_34Xcl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-b8f16575f5c816c3.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
